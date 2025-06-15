0973b7a3304788cc9d93d98bf977d5bb366e19bf
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 13:47:36 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')  # neophodno za GitHub
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
-    return driver
+    yield driver
     driver.quit()
