6d52e998ed92d877fa8efefdabd2110e5f87b102
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 15:58:27 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
@@ -10,7 +11,8 @@ def setup():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
