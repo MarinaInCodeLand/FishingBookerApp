12825196a31dd67b57e19cde34bd2ec5fa69388b
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 16:09:24 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,18 +1,20 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import pytest
 
 @pytest.fixture()
 def setup():
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument('--headless')  # za CI/CD bez GUI-ja
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
+    # Inicijalizuj ChromeDriver servis sa webdriver_manager-om
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+
     driver.maximize_window()
     yield driver
     driver.quit()
