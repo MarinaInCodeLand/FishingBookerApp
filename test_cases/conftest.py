fb4a6704e07c1466a339f4b66d05a42105ee9c28
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 17:10:33 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -4,18 +4,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture()
+@pytest.fixture
 def setup():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Ne prikazuje prozor browsera
+    chrome_options.add_argument("--headless")  # za CI
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager(version="116.0.5845.96").install()),
+        service=Service(ChromeDriverManager().install()),
         options=chrome_options
     )
 
-    driver.maximize_window()
     yield driver
     driver.quit()
+
