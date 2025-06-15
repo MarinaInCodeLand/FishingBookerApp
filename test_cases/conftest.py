debc7ff8b9a3aa216f753ddf03bb0ea22212370d
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 17:13:57 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -7,15 +7,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def setup():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # za CI
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service(ChromeDriverManager(use_cft=True).install()),
         options=chrome_options
     )
 
     yield driver
     driver.quit()
-
