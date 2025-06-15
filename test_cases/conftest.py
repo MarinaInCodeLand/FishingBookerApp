6d71fe350bb408e4cfa9841f12266d83d189a3c0
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 16:29:19 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -4,15 +4,16 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture
+@pytest.fixture()
 def setup():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
+    # KORISTI verziju ChromeDriver-a koja odgovara Chrome 115
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager(version="116.0.5845.96").install()),
+        service=Service(ChromeDriverManager(version="115.0.5790.170").install()),
         options=chrome_options
     )
 
