8a781dc5774121d0b954da7864e6a9ce76e5b5fa
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 16:53:15 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Ne prikazuje prozor browsera
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # koristi tačnu verziju ChromeDriver-a koja je instalirana u GitHub Actions
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager(version="116.0.5845.96").install()),
+        options=chrome_options
+    )
     driver.maximize_window()
-    return driver
+    yield driver
     driver.quit()
-
-
