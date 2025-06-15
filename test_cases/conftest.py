a8f490468c2152714b56c23a0ce154ee5e241b23
MarinaInCodeLand <134456169+MarinaInCodeLand@users.noreply.github.com>
2025-06-15 15:43:17 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,16 +1,11 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def setup():
-    options = Options()
-    options.binary_location = "/opt/google/chrome/google-chrome"
-    options.add_argument('--headless')  # neophodno za GitHub
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
